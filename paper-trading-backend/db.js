b0f03c6bfc7d5e86372cc627a3e27c1b802d77ba
Manav Shah <manavshah115@gmail.com>
2024-03-28 03:31:36 -0400

    .env

--- paper-trading-backend/db.js
+++ paper-trading-backend/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: "papertrading.mysql.database.azure.com",
-  user: "manavs",
-  password: "Vinny#2002",
-  database: "data",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
